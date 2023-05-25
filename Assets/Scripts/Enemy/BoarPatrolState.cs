4361cbaba86b6e47ecb9cd3e6e17e80cfe6464fd
chensicong <chensicong@konka.com>
2023-05-25 20:09:22 +0800

    statemachine

--- Assets/Scripts/Enemy/BoarPatrolState.cs
+++ Assets/Scripts/Enemy/BoarPatrolState.cs
@@ -7,13 +7,13 @@ public class BoarPatrolState : BaseState
     public override void OnEnter(Enemy enemy)
     {
         currentEnemy = enemy;
-
+        currentEnemy.currentSpeed = currentEnemy.normalSpeed;
     }
 
     public override void LoginUpdate()
     {
         //TODO:·¢ÏÖplayerÇÐ»»µ½chase×´Ì¬
-        if (!currentEnemy.physicsCheck.isGround||(currentEnemy.physicsCheck.touchLeftWall && currentEnemy.faceDir.x < 0) || (currentEnemy.physicsCheck.touchRightWall && currentEnemy.faceDir.x > 0))
+        if ((!currentEnemy.physicsCheck.isGround && currentEnemy.faceDir.x>0)||(currentEnemy.physicsCheck.touchLeftWall && currentEnemy.faceDir.x < 0) || (currentEnemy.physicsCheck.touchRightWall && currentEnemy.faceDir.x > 0))
         {
             currentEnemy.wait = true;
             currentEnemy.anim.SetBool("walk", false);
